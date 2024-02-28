156cd12ef16ff07f0f8a290b431754843b2129e4
Kate Basile <kathrynbasil5@gmail.com>
2024-02-28 11:17:46 -0500

    my changes

--- src/index.js
+++ src/index.js
@@ -1,29 +1,12 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import { useState } from "react";
-import StarRating from "./StarRating";
 
-function Test() {
-  const [movieRating, setMovieRating] = useState(0);
-  return (
-    <div>
-      <StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />
-      <p>{movieRating}</p>
-    </div>
-  );
-}
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <StarRating
-      maxRating={5}
-      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
-    />
-    <StarRating size={24} color="red" className="test" defaultRating={3} />
-
-    <Test />
+    <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
